05bfa879d0eb7ffaed2be037de6b5f588ca5b10a
Leticia <124852398+LeticiaBHB@users.noreply.github.com>
2023-08-01 00:01:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,58 @@
-import PyPDF2
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
-from collections import Counter
+import PyPDF4
+from translate import Translator
+from summa import summarizer, keywords
 from docx import Document
 
-nltk.download("punkt")
-nltk.download("stopwords")
-
 def read_pdf_text(filename):
+    pdf_text = ""
     with open(filename, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        text = ""
+        reader = PyPDF4.PdfReader(file)
         for page_num in range(len(reader.pages)):
             page = reader.pages[page_num]
-            text += page.extract_text()
-        return text
+            pdf_text += page.extract_text()
+    return pdf_text
 
-def preprocess_text(text):
-    # Remove caracteres especiais e números
-    text = re.sub(r'\d+', '', text)
-    text = re.sub(r'[^\w\s]', '', text)
+def translate_text(text, target_lang='pt'):
+    translator = Translator(to_lang=target_lang)
+    translation = translator.translate(text)
+    return translation if translation else "Erro na tradução."
 
-    # Tokenize as palavras e as sentenças
-    words = word_tokenize(text)
-    sentences = sent_tokenize(text)
+def create_summary_docx(text, output_docx):
+    summarized_text = summarizer.summarize(text)
 
-    # Remover stopwords (palavras irrelevantes)
-    stop_words = set(stopwords.words('english'))
-    words = [word.lower() for word in words if word.lower() not in stop_words]
-
-    return words, sentences
-
-def get_most_common_words(text, top_n=10):
-    words = re.findall(r'\b\w+\b', text.lower())
-    word_freq = Counter(words)
-    return word_freq.most_common(top_n)
-
-def summarize_text(sentences, ratio=0.3):
-    num_sentences = max(1, int(ratio * len(sentences)))
-    summarized_text = ' '.join(sentences[:num_sentences])
-    return summarized_text
-
-def create_summary_docx(summary_text, output_docx):
     doc = Document()
-    doc.add_heading('Resumo', level=1)
-    doc.add_paragraph(summary_text)
+    doc.add_paragraph(summarized_text)
+
     doc.save(output_docx)
+
+def create_summary_txt(text, output_txt):
+    summarized_text = summarizer.summarize(text)
+    keywords_text = keywords.keywords(text)
+
+    with open(output_txt, 'w', encoding='utf-8') as file:
+        file.write("Resumo:\n")
+        file.write(summarized_text)
+        file.write("\n\nPalavras-chave:\n")
+        file.write(keywords_text)
 
 if __name__ == "__main__":
     pdf_filename = "SOWB2022_EN_compressed.pdf"  # Altere o nome do arquivo aqui, se necessário.
-    output_filename = "resumo.docx"   # Nome do arquivo de saída do resumo em Word.
+    output_docx = "resumo.docx"  # Nome do arquivo de saída com o resumo em Word.
+    output_txt = "resumo.txt"  # Nome do arquivo de saída com o resumo em formato de texto.
 
     try:
         pdf_text = read_pdf_text(pdf_filename)
 
-        # Obter as palavras mais comuns no PDF
-        most_common_words = get_most_common_words(pdf_text)
-        print("Palavras mais repetidas no PDF:")
-        for word, freq in most_common_words:
-            print(f"{word}: {freq}")
+        # Traduzir o texto para o português
+        translated_text = translate_text(pdf_text, target_lang='pt')
 
-        # Resumir o texto do PDF
-        words, sentences = preprocess_text(pdf_text)
-        summarized_text = summarize_text(sentences)
+        # Criar o resumo em um arquivo Word
+        create_summary_docx(translated_text, output_docx)
 
-        # Criar o resumo em Word
-        create_summary_docx(summarized_text, output_filename)
+        # Criar o resumo em um arquivo de texto
+        create_summary_txt(translated_text, output_txt)
 
-        print(f"Resumo criado com sucesso e salvo em '{output_filename}'.")
+        print(f"Resumo traduzido criado com sucesso e salvo em '{output_docx}' e '{output_txt}'.")
     except FileNotFoundError:
         print(f"O arquivo '{pdf_filename}' não foi encontrado.")
     except Exception as e:
